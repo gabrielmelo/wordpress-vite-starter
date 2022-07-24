6c9f298ec75dbc6c6f59c2dd3514babc037ca75e
Gabriel Melo <gabriel.smelo@proton.me>
2022-07-24 04:57:52 -0300

    init project. clean structure

--- page.php
+++ page.php
@@ -1,12 +1,8 @@
 <?php get_header() ?>
-
-    <article class="max-w-screen-lg mx-auto ">
-
-        <h1 class="text-3xl font-semibold"><?php the_title() ?></h1>
-        <div class="mt-4">
-            <?php the_content() ?>
-        </div>
-
-    </article>
-
+	<article class="max-w-screen-lg mx-auto ">
+		<h1 class="text-3xl font-semibold"><?php the_title() ?></h1>
+		<div class="mt-4">
+				<?php the_content() ?>
+		</div>
+	</article>
 <?php get_footer() ?>
